04abfc54f33d61e996770f318fb1313ce985cbc9
Richard105247908 <105247908@aston.ac.uk>
2025-08-21 14:02:59 +0100

    employee class

---
+++ Employee.java
@@ -0,0 +1,19 @@
+package Leave_tracking_system;
+
+public class Employee {
+    // Properties (attributes)
+    private int employeeId;
+    private String name;
+    private String department;
+    private String email;
+
+    // Constructor
+    public Employee(int employeeId, String name, String department, String email) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.email = email;
+    }
+    // Methods will be added here
+}
+
