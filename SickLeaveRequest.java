3df5bba3fbbb77295b22c2a455bfde2113dd67fc
Richard105247908 <105247908@aston.ac.uk>
2025-08-28 16:18:55 +0100

    duration added as an attribute

--- SickLeaveRequest.java
+++ SickLeaveRequest.java
@@ -2,6 +2,7 @@ package Leave_tracking_system;
 
 public class SickLeaveRequest extends LeaveRequest {
     private boolean medicalCertificateProvided;
+    private int duration;
 
     public SickLeaveRequest(int requestId, Employee employee,
                             String startDate, String endDate,
@@ -12,5 +13,19 @@ public class SickLeaveRequest extends LeaveRequest {
 
     public boolean isMedicalCertificateProvided() {
         return medicalCertificateProvided;
+    }
+
+    public void setDuration(int duration) {this.duration = duration;}
+    public int getDuration() {return duration;}
+
+    // In the SickLeaveRequest class
+    @Override
+    public boolean processRequest() {
+        if (!medicalCertificateProvided && getDuration() > 2) {
+            System.out.println("Sick leave longer than 2 days requires a medical certificate");
+            return false;
+        }
+        System.out.println("Processing sick leave request...");
+        return true;
     }
 }
